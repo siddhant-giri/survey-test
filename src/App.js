369a7fce3b20afffef74dbee96d8f60f11ceac16
Siddhant Giri <sidgiri2000@gmail.com>
2020-06-08 09:57:54 -0700

    second commit

--- src/App.js
+++ src/App.js
@@ -1,150 +1,148 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 import 'survey-react/survey.css';
 import * as Survey from 'survey-react';
  
-class App extends Component {
-  constructor(props){
-    super(props)
-    this.state = {
+function App() {
 
-    }
-    this.onCompleteComponent = this.onCompleteComponent.bind(this)
-  }
-  onCompleteComponent = () => {
-    this.setState({
-      isCompleted : true
-    })
-  }
 
-  render() {
+  Survey
+  .StylesManager
+  .applyTheme("winterstone");
+  Survey.defaultBootstrapCss.navigationButton = "btn btn-green";
+
+
+
+  var surveyJSON = {
     
-    Survey
-    .StylesManager
-    .applyTheme("winterstone");
-Survey.defaultBootstrapCss.navigationButton = "btn btn-green";
-
-var json = {
-    
-    pages: [
-      {
-        
-      
-        // title: "How many people do visit your website everyday?",
-        questions: [
-            {
-                type: "radiogroup",
-                name: "firstqs",
-                title: "How many people do visit your website everyday?",
-                colCount:4,
-                hasOther: false,
-                isRequired: true,
-                choices: ["Less than 100", "101-1000", "1001-5000","More than 5000"]
-            }
+    "pages": [
+     {
+      "name": "page1",
+      "elements": [
+       {
+        "type": "radiogroup",
+        "name": "firstqs",
+        "title": "How many people do visit your website everyday?",
+        "isRequired": true,
+        "colCount":4,
+        "hasOther": false,
+        "choices": [
+          "Less than 100", 
+          "101-1000", 
+          "1001-5000",
+          "More than 5000"
         ]
-    },
-        {
-            
-            questions: [
-                {
-                    type: "checkbox",
-                    name: "secondqs",
-                    colCount:4,
-                    title: "What operating system do you use?",
-                    hasOther: true,
-                    isRequired: true,
-                    choices: ["Windows", "Linux", "Macintosh OSX"]
-                }
-            ]
-        }, {
-            
-            questions: [
-                {
-                    type: "checkbox",
-                    name: "langs",
-                    title: "What language(s) are you currently using?",
-                    colCount: 4,
-                    isRequired: true,
-                    choices: [
-                        "Javascript",
-                        "Java",
-                        "Python",
-                        "CSS",
-                    ]
-                }
-            ]
-        }, 
-        {
-          
-          questions: [
-              {
-                  type: "rating",
-                  name: "langs",
-                  title: "Rate Us",
-                  colCount: 4,
-                  isRequired: true,
-                  choicesLoaded: [
-                      "1","2","3","4"
-                  ]
-              }
-          ]
-      },
-        {
-            title: "Please enter your name and e-mail",
-            questions: [
-                {
-                    type: "text",
-                    name: "name",
-                    
-                }, {
-                    type: "text",
-                    name: "email",
-                    
-                }
-            ]
-        }
-    ]
-};
+       }
+      ],
+     // "title": "What operating system do you use?"
+     },
+     {
+      "name": "page2",
+      "elements": [
+       {
+        "type": "checkbox",
+        "name": "secondqs",
+        "title": "What language(s) are you currently using?",
+        "isRequired": true,
+        "choices": [
+         "Javascript",
+         "Java",
+         "Python",
+         "CSS",
+        ],
+        "colCount": 4
+       }
+      ],
+      //"title": "What language(s) are you currently using?"
+     },
+     {
+      "name": "page3",
+      "elements": [
+       {
+        "type": "checkbox",
+        "name": "thirdqs",
+        "title": "What operating system do you use?",
+        "isRequired": true,
+        "colCount":4,
+        "hasOther": false,
+        "choices": [
+          "Windows", 
+          "Linux", 
+          "Macintosh OSX",
+          "Other"
+        ]
+       }
+      ],
+     // "title": "What operating system do you use?"
+     },
+     {
+      "name": "page4",
+      "elements": [
+       {
+        "type": "rating",
+        "name": "fourthqs",
+        "title": "Rate Us",
+        "isRequired": true,
+        "colCount":4,
+        "hasOther": false,
+        "choicesLoaded": [
+          "1",
+          "2",
+          "3",
+          "4",
+          "5"
+        ]
+       }
+      ],
+     // "title": "What operating system do you use?"
+     },
+     {
+      "name": "page5",
+      "elements": [
+       {
+        "type": "text",
+        "name": "name",
+        "title": "Name:"
+       },
+       {
+        "type": "text",
+        "name": "email",
+        "title": "Your e-mail"
+       }
+      ],
+      "title": "Please enter your name and e-mail"
+     }
+    ],
+    "showProgressBar": "bottom"
+   }
+ 
+   function sendDataToServer(survey) {
+    //send Ajax request to your web server.
+    alert("The results are:" + JSON.stringify(survey.data));
+}
 
-// window.survey = new Survey.Model(json);
-
-// survey
-//     .onComplete
-//     .add(function (result) {
-//         document
-//             .querySelector('#surveyResult')
-//             .textContent = "Result JSON:\n" + JSON.stringify(result.data, null, 3);
-//     });
-
-// survey.showProgressBar = 'bottom';
-
-// ReactDOM.render(<Survey.Survey model={survey}/>, document.getElementById("surveyElement"));
-
- var surveyRender = !this.state.isCompleted ? (
-   <Survey.Survey 
-   json={json}
-   showCompletedPage={false}
-   onComplete={this.onCompleteComponent}
-   
-   />
- ) : null;
-
- var onSurveyCompletion= this.state.isCompleted ? (
- <div className="thanks"><p className="thanksPara">Thanks for completing the survey</p></div>
- ) : null;
-
-    return (
+  return (
       <div className="container">
-        <div className="para"><p>Please answer few questions, so that we can help you better</p></div>
 
-        <div className="survey">
-          <div className="top">Quick Survey</div>
-          {surveyRender}
-          {onSurveyCompletion}
-        </div>
+    <div><p className="para">Please answer dew questions, so that we can help better.</p></div>
+
+
+<div className="survey">
+<div className="top">Quick Survey</div>
+      <Survey.Survey json={ surveyJSON } onComplete={ sendDataToServer } />
+      </div>
+        {/* // <div className="para"><p>Please answer few questions, so that we can help you better</p></div>
+
+        // <div className="survey" id="surveyElement">
+        //   <div className="top" >Quick Survey</div>
+        //   {surveyRender}
+        //   {onSurveyCompletion} 
+          
+        // </div>
+        // <div className="surveyResult"></div> */}
       </div>
     );
-  }
+  
 }
  
 export default App;
